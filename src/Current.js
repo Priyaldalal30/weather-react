8525b86f7fab97f41f886606f0c7587e07293ac7
Priyaldalal30 <112647678+Priyaldalal30@users.noreply.github.com>
2022-10-29 15:37:42 +0100

    edited css and added more temp data

--- src/Current.js
+++ src/Current.js
@@ -7,7 +7,8 @@ export default function City() {
     city: "Woking",
     date: "Tuesday 25th October",
     time: "15:30",
-    description: "cloudy",
+    description1: "rain",
+    description2: "light rain",
     hightemp: "19",
     lowtemp: "12",
     imgUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
@@ -16,6 +17,7 @@ export default function City() {
     humidity: "74",
     wind: "8",
     visibility: "10",
+    feelsLike: "15",
   };
   return (
     <div className="row">
@@ -23,24 +25,9 @@ export default function City() {
         <div className="weathericon">
           <img src={weatherData.imgUrl} alt="weathericon" />
         </div>
-        <div className="description">{weatherData.description}</div>
-        <h5>
-          <div className="high-low-temp">
-            H: <span>{weatherData.hightemp}</span>
-            <span> °C | L: </span>
-            <span>{weatherData.lowtemp}</span>
-            <span> °C</span>
-          </div>
-          <div>
-            <div className="sun-time">
-              Sunrise:
-              <span> {weatherData.sunrise}</span>
-            </div>
-            <div className="sun-time">
-              Sunset: <span>{weatherData.sunset}</span>
-            </div>
-          </div>
-        </h5>
+        <div className="description">{weatherData.description1}</div>
+        <br />
+        <div className="description">{weatherData.description2}</div>
       </div>
       <div className="col-4">
         <div className="city-card">
@@ -65,22 +52,43 @@ export default function City() {
       <div className="col-4">
         <div className="details">
           <br />
-          <div>
-            <i className="fa-solid fa-droplet"></i> Humidity:
-            <span className="humidity"></span>{" "}
-            <span className="unit">{weatherData.humidity}%</span>
+          <div className="details1">
+            <i className="fa-solid fa-droplet"></i> Humidity:{" "}
+            <span>{weatherData.humidity}%</span>
           </div>
-          <br />
-          <div>
+
+          <div className="details2">
             <i className="fa-solid fa-wind"></i> Wind:{" "}
-            <span className="wind"></span>
-            <span className="unit">{weatherData.wind} mph</span>
+            <span>{weatherData.wind}mph</span>
           </div>
-          <br />
-          <div>
-            <i className="fa-solid fa-eye"></i> Visibility:
-            <span className="visibility"></span>
-            <span className="unit">{weatherData.visibility} mi</span>
+
+          <div className="details3">
+            <i className="fa-solid fa-eye"></i> Visibility:{" "}
+            <span>{weatherData.visibility}mi</span>
+          </div>
+          <div className="details4">
+            <i class="fa-solid fa-temperature-quarter"></i> Feels Like:{" "}
+            <span>{weatherData.feelsLike}°C</span>
+          </div>
+
+          <div className="details5">
+            <i class="fa-solid fa-temperature-high"></i> High:{" "}
+            <span>{weatherData.hightemp}°C</span>
+          </div>
+
+          <div className="details6">
+            <i class="fa-solid fa-temperature-low"></i> Low:{" "}
+            <span>{weatherData.lowtemp}°C</span>
+          </div>
+
+          <div className="details7">
+            <i class="fa-regular fa-sun"></i> Sunrise:{" "}
+            <span>{weatherData.sunrise}</span>
+          </div>
+
+          <div className="details8">
+            <i class="fa-regular fa-moon"></i> Sunset:{" "}
+            <span>{weatherData.sunset}</span>
           </div>
         </div>
       </div>
