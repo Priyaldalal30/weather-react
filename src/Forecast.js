d61e56b5b09568dfe313e667b1300fd67df3802e
Priyaldalal30 <112647678+Priyaldalal30@users.noreply.github.com>
2022-10-29 15:35:33 +0100

    edited css and added more data for temp.

--- src/Forecast.js
+++ src/Forecast.js
@@ -5,6 +5,8 @@ import "./Forecast.css";
 export default function Weather() {
   return (
     <div>
+      <br />
+      <br />
       <div className="hourly-card">Hourly Forecast....</div>
       <div className="daily-card">Daily Forecast....</div>
     </div>
