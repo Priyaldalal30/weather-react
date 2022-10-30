495665045795d317c35036077c996ca1ad55533f
Priyaldalal30 <112647678+Priyaldalal30@users.noreply.github.com>
2022-10-30 21:49:07 +0000

    edited css and layout for small screen - working on making app responsiv…

--- src/Forecast.js
+++ src/Forecast.js
@@ -7,8 +7,23 @@ export default function Weather() {
     <div>
       <br />
       <br />
-      <div className="hourly-card">Hourly Forecast....</div>
-      <div className="daily-card">Daily Forecast....</div>
+      <div className="hourly-card">
+        <div className="card-day-1">6 - 9 </div>
+        <div className="card-day-2">9 - 12 </div>
+        <div className="card-day-3">12 - 15 </div>
+        <div className="card-day-4">15 - 18 </div>
+        <div className="card-day-5">18 - 21 </div>
+        <div className="card-day-6">21 - 12 </div>
+      </div>
+      <br />
+      <div className="daily-grid">
+        <div className="card-day-1">Monday </div>
+        <div className="card-day-2">Tuesday </div>
+        <div className="card-day-3">Wednesday </div>
+        <div className="card-day-4">Thursday </div>
+        <div className="card-day-5">Friday </div>
+        <div className="card-day-6">Saturday </div>
+      </div>
     </div>
   );
 }
