3fdb0bfd9b88bf15af9295ae675d8809f814668c
Priyaldalal30 <112647678+Priyaldalal30@users.noreply.github.com>
2022-11-01 21:44:02 +0000

    added linear background colour, added daily forecast

--- src/DailyForecast.js
+++ src/DailyForecast.js
@@ -1,38 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import DailyForecastDay from "./DailyForecastDay";
 
 import "./DailyForecast.css";
-import WeatherIcon from "./WeatherIcon";
 
 export default function Weather(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecastDay, setForecastDate] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   function showDailyForecast(response) {
-    console.log(response.data);
+    setForecastDate(response.data.daily);
+    setLoaded(true);
   }
 
-  let apiKey = "57821c3b75b60c68ecd1a8d0dd1aa8d3";
-  let latitude = props.coordinates.coordinates.lat;
-  let longitude = props.coordinates.coordinates.lon;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showDailyForecast);
-
-  return (
-    <div>
+  if (loaded) {
+    console.log(forecastDay);
+    return (
       <div className="daily-grid">
-        <div className="card-day-1">
-          <div className="forecast-day">Mon</div>
-          <div className="forecast-icon">
-            <WeatherIcon
-              code="01n"
-              size={45}
-              color={"rgba(247, 243, 243, 0.655)"}
-            />
-          </div>
-          <div className="forecast-temp">
-            <span className="forecast-high-temp">19°</span>
-            <span className="forecast-low-temp">12°</span>
-          </div>
+        <div>
+          <DailyForecastDay data={forecastDay[1]} />
+        </div>
+        <div>
+          <DailyForecastDay data={forecastDay[2]} />
+        </div>
+        <div>
+          <DailyForecastDay data={forecastDay[3]} />
+        </div>
+        <div>
+          <DailyForecastDay data={forecastDay[4]} />
+        </div>
+        <div>
+          <DailyForecastDay data={forecastDay[5]} />
+        </div>
+        <div>
+          <DailyForecastDay data={forecastDay[6]} />
+        </div>
+        <div>
+          <DailyForecastDay data={forecastDay[7]} />
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "57821c3b75b60c68ecd1a8d0dd1aa8d3";
+    let latitude = props.coordinates.coordinates.lat;
+    let longitude = props.coordinates.coordinates.lon;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showDailyForecast);
+    return null;
+  }
 }
---
+++ src/DailyForecastDay.js
@@ -0,0 +1,40 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+
+export default function DailyForecastDay(props) {
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ];
+    return days[day];
+  }
+  return (
+    <div className="card-day-1">
+      <div className="forecast-day">{day()}</div>
+      <div className="forecast-icon">
+        <WeatherIcon
+          code={props.data.weather[0].icon}
+          size={40}
+          color={"rgba(247, 243, 243, 0.655)"}
+        />
+      </div>
+      <div className="forecast-temp">
+        <span className="forecast-high-temp">
+          {Math.round(props.data.temp.max)}°
+        </span>
+        <span className="forecast-low-temp">
+          {Math.round(props.data.temp.min)}°
+        </span>
+      </div>
+    </div>
+  );
+}
