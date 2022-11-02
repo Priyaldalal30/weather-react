6e1611a2954ba015e2a5e1282fa03dfb41570c40
Priyaldalal30 <112647678+Priyaldalal30@users.noreply.github.com>
2022-11-02 08:50:10 +0000

    added time for hourly forecast

--- src/HourlyForecastHour.js
+++ src/HourlyForecastHour.js
@@ -2,16 +2,26 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function HourlyForecastHour(props) {
+  function time() {
+    let date = new Date(props.data.dt * 1000);
+    let hour = date.getHours();
+    return hour;
+  }
+
   return (
     <div className="card-hour-1">
+      <div className="forecast-current-hour">{time()}</div>
+
       <div className="forecast-icon">
         <WeatherIcon
           code={props.data.weather[0].icon}
-          size={40}
+          size={25}
           color={"rgba(247, 243, 243, 0.655)"}
         />
       </div>
-      <div className="forecast-current-temp">{Math.round(props.data.temp)}</div>
+      <div className="forecast-current-temp">
+        {Math.round(props.data.temp)}°
+      </div>
     </div>
   );
 }
